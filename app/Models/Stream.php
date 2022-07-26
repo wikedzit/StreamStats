299307b550dde0313edb81f64b42ab0bad64d8f2
Timothy Wikedzi <timothy.wikedzi@andela.com>
2022-07-27 00:27:37 +0300

    change primary key referencing for Stream model

--- app/Models/Stream.php
+++ app/Models/Stream.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Stream extends Model
 {
     use HasFactory;
+
+    public $incrementing    = false;
+    protected $primaryKey   = 'stream_id';
+    protected $keyType      = 'string';
+
+    protected $dates = [
+        'started_at'
+    ];
+
+    protected $fillable = [
+        'stream_id', 'game_name', 'title', 'viewer_count', 'started_at', 'channel_name', 'tag_ids'
+    ];
+
 }
