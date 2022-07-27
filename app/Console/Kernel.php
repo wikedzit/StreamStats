41867270426a7d2427e226e3303f21edaee60f9c
Timothy Wikedzi <timothy.wikedzi@andela.com>
2022-07-28 02:31:14 +0300

    register and schedule the update command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,16 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateStream;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        UpdateStream::class
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +20,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('stream:update')->everyFifteenMinutes();
     }
 
     /**
