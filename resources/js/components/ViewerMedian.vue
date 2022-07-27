ab27e09b1a4b48c0caa468dd4fcc936a8cf22985
Timothy Wikedzi <timothy.wikedzi@andela.com>
2022-07-27 21:29:22 +0300

    update dashboard page to accomodate stats components

---
+++ resources/js/components/ViewerMedian.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    props: ['median'],
+    data() {
+        return {
+            viewer_median: null,
+        }
+    },
+    created() {
+        this.viewer_median = this.median
+    },
+    methods: {
+    }
+}
+</script>
+<template>
+    <div class="card bg-info">
+        <div class="card-body">
+            <h5 class="card-title">Stream Viewers</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Median</h6>
+            <div class="card-text">
+                <h1>{{  viewer_median  }}</h1>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,3 +1,30 @@
+<script>
+import ViewerMedian from "../components/ViewerMedian.vue";
+
+export default {
+    components: {
+        ViewerMedian
+    },
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+
+    methods: {
+    }
+}
+</script>
 <template>
-    <h1>Dashboard</h1>
+    <div class="container h-100 d-flex justify-content-center align-items-center">
+        <div class="container w-70">
+            <h3>DashBoard</h3>
+            <div class="row mb-10">
+                <div class="col-6">
+                    <ViewerMedian :median="567"></ViewerMedian>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
