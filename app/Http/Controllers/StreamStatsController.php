b83484a4bc35e6a44875c030665468b14e64ea91
Timothy Wikedzi <timothy.wikedzi@andela.com>
2022-07-29 02:27:44 +0300

    add favicon

--- app/Http/Controllers/StreamStatsController.php
+++ app/Http/Controllers/StreamStatsController.php
@@ -11,6 +11,8 @@ class StreamStatsController extends Controller
     public function getStats() {
         $stats = Stream::getStats();
         if (Cache::get('updating_streams')) {
+            // TODO at this point we can even retry N times before sending feedback to user
+            // This logic could even be places in a Middleware
             $stats = ["message" => "loading"];
         }
         return response()->json($stats);
