512021c9a37f8f32be6f9ecf317ecfa5910d021d
Timothy Wikedzi <timothy.wikedzi@andela.com>
2022-07-29 03:25:30 +0300

    update landing page and add variables to .env example

--- resources/js/pages/Landing.vue
+++ resources/js/pages/Landing.vue
@@ -11,31 +11,15 @@
                 <div class="p-5 mb-4 bg-light rounded-3">
                     <div class="container-fluid py-5">
                         <h1 class="display-5 fw-bold">Get Access to your stats</h1>
-                        <p class="col-md-8 fs-4">
+                        <p class="col-md-12 fs-4 text-align-center">
                             Have a quick look at how the channels you watch compare to the top 1000 Twitch live streams.
                         </p>
-                        <a v-if="!loading" :href="getRedirectUrl()" class="btn btn-primary btn-lg" type="button">
+                        <a v-if="!loading" :href="getRedirectUrl()" class="btn btn-lg btn-primary fs-4" type="button"
+                           style="background-color: #d162ff">
                             Sign in with Twitch
                         </a>
                         <div  v-if="loading" class="spinner-border text-primary" role="status">
                             <span class="visually-hidden">Loading...</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row align-items-md-stretch">
-                    <div class="col-md-6">
-                        <div class="h-100 p-5 text-bg-dark rounded-3">
-                            <h2>Change the background</h2>
-                            <p>Swap the background-color utility and add a `.text-*` color utility to mix up the jumbotron look. Then, mix and match with additional component themes and more.</p>
-                            <button class="btn btn-outline-light" type="button">Example button</button>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="h-100 p-5 bg-light border rounded-3">
-                            <h2>Add borders</h2>
-                            <p>Or, keep it light and add a border for some added definition to the boundaries of your content. Be sure to look under the hood at the source HTML here as we've adjusted the alignment and sizing of both column's content for equal-height.</p>
-                            <button class="btn btn-outline-secondary" type="button">Example button</button>
                         </div>
                     </div>
                 </div>
